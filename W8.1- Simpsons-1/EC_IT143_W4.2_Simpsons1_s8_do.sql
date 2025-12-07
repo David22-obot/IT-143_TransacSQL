1929d9f35f8328e8246c9cd1be6160b10b71db41
David22-obot <oboteck@gmail.com>
2025-12-07 17:46:02 +0100

    Updating

--- W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s8_do.sql
+++ W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s8_do.sql
@@ -1,1 +1,6 @@
-EXEC sp_load_Simpsons1_family_character_count_do;
+/*
+    EC_IT143_W4.2_hello_world_s8_do.sql
+    Step 8: Call the stored procedure.
+*/
+
+EXEC Simpsons.dbo.spLoadTransactionDateRange;
