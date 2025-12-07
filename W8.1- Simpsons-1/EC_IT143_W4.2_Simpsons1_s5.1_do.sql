ca46c073b297976dcf819a83239be70ab1a536ea
David22-obot <oboteck@gmail.com>
2025-12-07 17:41:38 +0100

    Updating

--- W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s5.1_do.sql
+++ W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s5.1_do.sql
@@ -1,3 +1,13 @@
+/*
+    EC_IT143_W4.2_hello_world_s5.1_do.sql
+    Step 5.1: Create a table from view using SELECT INTO.
+*/
+
+IF OBJECT_ID('Simpsons.dbo.tblTransactionDateRange', 'U') IS NOT NULL
+    DROP TABLE Simpsons.dbo.tblTransactionDateRange;
+GO
+
 SELECT *
-INTO tbl_Simpsons1_family_character_count_do
-FROM vw_Simpsons1_family_character_count_do;
+INTO Simpsons.dbo.tblTransactionDateRange
+FROM Simpsons.dbo.vwTransactionDateRange;
+GO
