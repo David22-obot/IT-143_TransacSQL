fd8e344166ed4f40b6158e25a088598304e0dd09
David22-obot <oboteck@gmail.com>
2025-12-07 17:23:22 +0100

    Updating
    Refined table architecture by adding primary key and constraints.

--- W8.1- MyFC-1/EC_IT143_W4.2_MyFC1_s5_do.sql
+++ W8.1- MyFC-1/EC_IT143_W4.2_MyFC1_s5_do.sql
@@ -1,3 +1,15 @@
-SELECT *
-INTO tbl_MyFC1_team_player_count_do
-FROM vw_MyFC1_team_player_count_do;
+/*
+    EC_IT143_W4.2_hello_world_s5.2_do.sql
+    Step 5.2: Refine table architecture.
+*/
+
+-- Add primary key
+ALTER TABLE MyFC.dbo.tblPlayerCountByTeam
+ADD CONSTRAINT PK_tblPlayerCountByTeam PRIMARY KEY (t_id);
+
+-- Add constraints
+ALTER TABLE MyFC.dbo.tblPlayerCountByTeam
+ALTER COLUMN t_id INT NOT NULL;
+
+ALTER TABLE MyFC.dbo.tblPlayerCountByTeam
+ALTER COLUMN total_players INT NOT NULL;
