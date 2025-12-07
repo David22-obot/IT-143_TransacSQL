be2d3719c528c6f1e178d110350a7ddd3cc4ae25
David22-obot <oboteck@gmail.com>
2025-12-07 17:40:39 +0100

    Updating

--- W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s4_do.sql
+++ W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s4_do.sql
@@ -1,4 +1,16 @@
-CREATE OR ALTER VIEW vw_Simpsons1_family_character_count_do AS
-SELECT family_name, COUNT(*) AS character_count
-FROM Simpsons1_Characters
-GROUP BY family_name;
+/*
+    EC_IT143_W4.2_hello_world_s4_do.sql
+    Step 4: Turn the ad hoc SQL query into a view.
+*/
+
+IF OBJECT_ID('Simpsons.dbo.vwTransactionDateRange', 'V') IS NOT NULL
+    DROP VIEW Simpsons.dbo.vwTransactionDateRange;
+GO
+
+CREATE VIEW Simpsons.dbo.vwTransactionDateRange
+AS
+SELECT
+    MIN([Date]) AS earliest_transaction,
+    MAX([Date]) AS latest_transaction
+FROM Simpsons.dbo.FBS_Viza_Costmo;
+GO
