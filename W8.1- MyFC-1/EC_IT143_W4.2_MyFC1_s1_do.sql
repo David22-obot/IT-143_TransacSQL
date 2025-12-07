dc7ade50e052fec31ee624abc8205546c10c904a
David22-obot <oboteck@gmail.com>
2025-12-07 17:15:14 +0100

    Updating

--- W8.1- MyFC-1/EC_IT143_W4.2_MyFC1_s1_do.sql
+++ W8.1- MyFC-1/EC_IT143_W4.2_MyFC1_s1_do.sql
@@ -1,2 +1,8 @@
--- Q1: How many players are on each team?
--- A1: We will count players grouped by team.
+/*
+    EC_IT143_W4.2_hello_world_s1_do.sql
+    Step 1: Start with a question.
+    Question: How many players belong to each team in MyFC.dbo.tblPlayerDim?
+*/
+
+-- Simple question:
+-- How many players are on each team in the MyFC community database?
