d20b7ed815158c5f2154b7ca81fb098c5961c0bd
David22-obot <oboteck@gmail.com>
2025-12-07 17:42:28 +0100

    Updating

--- W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s5.2_do.sql
+++ W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s5.2_do.sql
@@ -1,7 +1,18 @@
-DROP TABLE IF EXISTS tbl_Simpsons1_family_character_count_do;
+/*
+    EC_IT143_W4.2_hello_world_s5.2_do.sql
+    Step 5.2: Refine the table architecture.
+*/
 
-CREATE TABLE tbl_Simpsons1_family_character_count_do (
-    family_name NVARCHAR(100) NOT NULL,
-    character_count INT NOT NULL,
-    CONSTRAINT pk_Simpsons1_families PRIMARY KEY (family_name)
-);
+-- Add a surrogate primary key.
+ALTER TABLE Simpsons.dbo.tblTransactionDateRange
+ADD id INT IDENTITY(1,1) NOT NULL;
+
+ALTER TABLE Simpsons.dbo.tblTransactionDateRange
+ADD CONSTRAINT PK_tblTransactionDateRange PRIMARY KEY (id);
+
+-- Make date fields NOT NULL.
+ALTER TABLE Simpsons.dbo.tblTransactionDateRange
+ALTER COLUMN earliest_transaction DATETIME NOT NULL;
+
+ALTER TABLE Simpsons.dbo.tblTransactionDateRange
+ALTER COLUMN latest_transaction DATETIME NOT NULL;
