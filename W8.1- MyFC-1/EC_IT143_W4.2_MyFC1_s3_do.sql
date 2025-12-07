5ea0d678426237c55075794a28b109292a848c19
David22-obot <oboteck@gmail.com>
2025-12-07 17:19:14 +0100

    Revise SQL query to count players by team IDupdating
    Updated SQL query to count players from tblPlayerDim.

--- W8.1- MyFC-1/EC_IT143_W4.2_MyFC1_s3_do.sql
+++ W8.1- MyFC-1/EC_IT143_W4.2_MyFC1_s3_do.sql
@@ -1,3 +1,12 @@
-SELECT team_name, COUNT(*) AS player_count
-FROM MyFC1_Players
-GROUP BY team_name;
+/*
+    EC_IT143_W4.2_hello_world_s3_do.sql
+    Step 3: Create an ad hoc SQL query.
+*/
+
+-- Count the number of players on each team.
+
+SELECT 
+    t_id,
+    COUNT(*) AS total_players
+FROM MyFC.dbo.tblPlayerDim
+GROUP BY t_id;
