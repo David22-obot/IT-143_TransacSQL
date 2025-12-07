1a14c7da66a304af1aed255d03467a4e2430287c
David22-obot <oboteck@gmail.com>
2025-12-07 17:39:24 +0100

    Updating
    Updated SQL query to calculate earliest and latest transaction dates.

--- W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s3_do.sql
+++ W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s3_do.sql
@@ -1,3 +1,11 @@
-SELECT family_name, COUNT(*) AS character_count
-FROM Simpsons1_Characters
-GROUP BY family_name;
+/*
+    EC_IT143_W4.2_hello_world_s3_do.sql
+    Step 3: Create an ad hoc SQL query.
+*/
+
+-- Calculate earliest and latest transaction date.
+
+SELECT
+    MIN([Date]) AS earliest_transaction,
+    MAX([Date]) AS latest_transaction
+FROM Simpsons.dbo.FBS_Viza_Costmo;
