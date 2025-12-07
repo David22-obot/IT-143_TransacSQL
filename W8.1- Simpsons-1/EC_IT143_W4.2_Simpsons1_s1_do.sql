0a335be21dd91e4a5725fd64e762f53197b43acb
David22-obot <oboteck@gmail.com>
2025-12-07 17:37:58 +0100

    Updating
    Added comments to clarify the purpose of the SQL script.

--- W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s1_do.sql
+++ W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s1_do.sql
@@ -1,2 +1,8 @@
--- Q1: How many characters are in each family group?
--- A1: We will group characters by family_name.
+/*
+    EC_IT143_W4.2_hello_world_s1_do.sql
+    Step 1: Start with a question.
+    Question: What is the earliest and latest transaction date in Simpsons.dbo.FBS_Viza_Costmo?
+*/
+
+-- Simple question:
+-- What is the earliest and the latest Date value in the table Simpsons.dbo.FBS_Viza_Costmo?
