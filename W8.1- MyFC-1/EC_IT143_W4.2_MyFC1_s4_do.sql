7f2c089f6e3eef2cc4aefc027c7b4668d9da6575
David22-obot <oboteck@gmail.com>
2025-12-07 17:21:25 +0100

    Updating

--- W8.1- MyFC-1/EC_IT143_W4.2_MyFC1_s4_do.sql
+++ W8.1- MyFC-1/EC_IT143_W4.2_MyFC1_s4_do.sql
@@ -1,4 +1,17 @@
-CREATE OR ALTER VIEW vw_MyFC1_team_player_count_do AS
-SELECT team_name, COUNT(*) AS player_count
-FROM MyFC1_Players
-GROUP BY team_name;
+/*
+    EC_IT143_W4.2_hello_world_s4_do.sql
+    Step 4: Create a view from the ad hoc query.
+*/
+
+IF OBJECT_ID('MyFC.dbo.vwPlayerCountByTeam', 'V') IS NOT NULL
+    DROP VIEW MyFC.dbo.vwPlayerCountByTeam;
+GO
+
+CREATE VIEW MyFC.dbo.vwPlayerCountByTeam
+AS
+SELECT 
+    t_id,
+    COUNT(*) AS total_players
+FROM MyFC.dbo.tblPlayerDim
+GROUP BY t_id;
+GO
