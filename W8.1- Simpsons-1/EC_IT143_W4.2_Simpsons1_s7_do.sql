d5e0a3ba0b0402c5141bd0bf85b32a9ba3c7ff02
David22-obot <oboteck@gmail.com>
2025-12-07 17:45:08 +0100

    Updating
    Created a stored procedure to load transaction date range into the specified table.

--- W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s7_do.sql
+++ W8.1- Simpsons-1/EC_IT143_W4.2_Simpsons1_s7_do.sql
@@ -1,9 +1,21 @@
-CREATE OR ALTER PROCEDURE sp_load_Simpsons1_family_character_count_do
+/*
+    EC_IT143_W4.2_hello_world_s7_do.sql
+    Step 7: Create stored procedure to load table.
+*/
+
+IF OBJECT_ID('Simpsons.dbo.spLoadTransactionDateRange', 'P') IS NOT NULL
+    DROP PROCEDURE Simpsons.dbo.spLoadTransactionDateRange;
+GO
+
+CREATE PROCEDURE Simpsons.dbo.spLoadTransactionDateRange
 AS
 BEGIN
-    TRUNCATE TABLE tbl_Simpsons1_family_character_count_do;
+    SET NOCOUNT ON;
 
-    INSERT INTO tbl_Simpsons1_family_character_count_do
-    SELECT *
-    FROM vw_Simpsons1_family_character_count_do;
+    TRUNCATE TABLE Simpsons.dbo.tblTransactionDateRange;
+
+    INSERT INTO Simpsons.dbo.tblTransactionDateRange (earliest_transaction, latest_transaction)
+    SELECT earliest_transaction, latest_transaction
+    FROM Simpsons.dbo.vwTransactionDateRange;
 END;
+GO
